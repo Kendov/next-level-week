cac50787eee8b7b1f5a8b2c6482114a4b25932b4
Cesar <cayocesar.ac@gmail.com>
2020-06-05 23:12:54 -0300

    environment variables

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,15 +1,22 @@
 import {Request, Response} from "express";
 import knex from "../database/connection";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
 
 class ItemsController{
+    
     async index (req: Request, res: Response) {
+        const uri = process.env.SERVER_URI;
         const items = await knex("items").select("*");
     
         const serializedItems = items.map(item => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:7000/uploads/${item.image}`
+                image_url: `http://${uri}/uploads/${item.image}`
             }
         });
         
